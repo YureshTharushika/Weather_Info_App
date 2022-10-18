82cf014571c8684092ce8204fba582035d5725f2
Yuresh Tharushika <yuresh.official@gmail.com>
2022-10-18 20:41:30 +0530

    API key moved to .env

--- api.py
+++ api.py
@@ -1,16 +1,17 @@
 
-
+import os
+from dotenv import load_dotenv
 import requests
 
-
-API_KEY = "a85e873a6179972729601795cb733467"
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 
 def get_weather(city):
     url = f"{BASE_URL}?appid={API_KEY}&q={city}"
     response = requests.get(url)
-
+    print(API_KEY)
     if response.status_code == 200:
         return response
     else:
