9b4f4abe3db78d3078fdf3d3ed50a1fbbd2198f9
Yuresh Tharushika <yuresh.official@gmail.com>
2022-10-18 20:10:53 +0530

    api endpoint added

--- api.py
+++ api.py
@@ -1,1 +1,17 @@
-API_KEY = a85e873a6179972729601795cb733467
+
+
+import requests
+
+
+API_KEY = "a85e873a6179972729601795cb733467"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+def get_weather(city):
+    url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    else:
+        return 1
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import string
 from fastapi import FastAPI
+
+from api import get_weather
 
 app = FastAPI()
 
@@ -6,3 +9,15 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+
+@app.get("/weather/{city}")
+def city_weather(city: str):
+
+    response = get_weather(city)
+    if not response == 1:
+        data = response.json()
+    else:
+        data = "An error occurred"
+
+    return data
