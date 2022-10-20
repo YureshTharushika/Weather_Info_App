79209369ff156f292f1472d4c66abb73bd4fe5cc
Yuresh Tharushika <yuresh.official@gmail.com>
2022-10-21 00:42:41 +0530

    weather report completed

--- api.py
+++ api.py
@@ -2,17 +2,47 @@
 import os
 from dotenv import load_dotenv
 import requests
+import tzlocal
+from datetime import datetime
+
 
 load_dotenv()
-API_KEY = os.environ.get("API_KEY")
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
+WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+GOOGLE_BASE_URL = "https://maps.googleapis.com/maps/api/place/"
 
 
 def get_weather(city):
-    url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+    url = f"{WEATHER_BASE_URL}?appid={WEATHER_API_KEY}&q={city}"
     response = requests.get(url)
-    print(API_KEY)
+
     if response.status_code == 200:
         return response
     else:
         return 1
+
+
+# def get_place_image(lat, lon, city):
+#     # placeurl = f"{GOOGLE_BASE_URL}nearbysearch/json?location={lat}%2C{lon}&radius=1500&type=city&keyword={city}&key={GOOGLE_API_KEY}"
+#     # placeresponse = requests.get(placeurl)
+#     # placedata = placeresponse.json()
+#     # photo_reference = placedata["results"]
+#     # print(placeresponse)
+#     # photourl = f"{GOOGLE_BASE_URL}photo/?maxwidth=400&photo_reference={photo_reference}&key={GOOGLE_API_KEY}"
+#     # photoresponse = requests.get(photourl)
+
+#     gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+#     places_result = gmaps.places_nearby(
+#         location=f"{lat},{lon}", radius=1500, type=city)
+#     print(places_result)
+#     return "places_result"
+
+
+def get_time(dt):
+    unix_timestamp = float(dt)
+    local_timezone = tzlocal.get_localzone()
+    local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+    return local_time.strftime("%H:%M:%S")
--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-import string
-from fastapi import FastAPI
 
-from api import get_weather
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from api import get_time, get_weather
 
 app = FastAPI()
 
-
-@app.get("/")
-def home():
-    return {"Data": "Test"}
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/weather/{city}")
-def city_weather(city: str):
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/weather", response_class=HTMLResponse)
+def city_weather(request: Request, city: str):
 
     response = get_weather(city)
+
     if not response == 1:
         data = response.json()
+        temperature = data["main"]["temp"]
+        weather = data["weather"][0]["description"]
+        icon = data["weather"][0]["icon"]
+        humidity = data["main"]["humidity"]
+        windspeed = data["wind"]["speed"]
+        clouds = data["clouds"]["all"]
+        lat = data["coord"]["lat"]
+        lon = data["coord"]["lon"]
+        time = get_time(data["dt"])
+        # photo = get_place_image(lat, lon, city)
+
+        # f = open('Image.jpg', 'wb')
+
+        # for chunk in photo:
+        #     if chunk:
+        #         f.write(chunk)
+        # f.close()
     else:
         data = "An error occurred"
 
-    return data
+    return templates.TemplateResponse("result.html", {"request": request, "temperature": round(temperature-273.15, 2), "weather": weather.capitalize(), "humidity": humidity, "windspeed": windspeed, "clouds": clouds, "time": time, "icon": icon, "city": city.capitalize()})
